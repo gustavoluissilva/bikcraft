dafdebfaef92897ae7ac82b41a734b3bf14f79a6
GUSTAVO LUIS DA SILVA <gustavoluis.silva@pmc.local>
2025-02-25 16:15:22 -0300

    commit 25-02-2025 colocada novas funcionalidades JS

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// Ativar Itens Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro)
+  if(elemento){
+    elemento.checked = true
+  }
+  
+}
+
+parametros.forEach(ativarProduto);
+
+// Ativar Links do Menu
+
+const links = document.querySelectorAll(".header-menu a")
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href
+  if (url.includes(href)) {
+
+    link.classList.add("ativo")
+
+  }
+
+}
+
+links.forEach(ativarLink);
+
+
+// Perguntas Frequentes 
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  pergunta.setAtribute("aria-expanded", "true");
+
+
+  resposta.classList.toggle('ativa')
+}
+
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener("click", ativarPergunta); 
+}
+
+perguntas.forEach(eventosPerguntas);
